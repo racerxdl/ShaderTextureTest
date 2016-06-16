d730b152e763c4938c79ea6f5b7f2f60992641a2
Lucas Teske <lucas@teske.net.br>
2016-06-15 23:07:44 -0300

    Base Test

---
+++ ShaderTextureTest/Content/ShaderTest.fx
@@ -0,0 +1,30 @@
+﻿#if OPENGL
+	#define SV_POSITION POSITION
+	#define VS_SHADERMODEL vs_3_0
+	#define PS_SHADERMODEL ps_3_0
+#else
+	#define VS_SHADERMODEL vs_4_0_level_9_1
+	#define PS_SHADERMODEL ps_4_0_level_9_1
+#endif
+
+sampler TextureSampler : register(s0);
+float time;
+float2 resolution;
+
+float4 main(float4 pos : SV_Position, float4 color : COLOR0, float2 texCoord : TEXCOORD0) : COLOR0
+{
+    float2 p = (pos.xy / resolution.xy) - 0.5;
+    float sx = 0.2* (p.x + 0.5) * sin( 50.0 * p.x - 10.f * time);
+    float dy = 1.f/ ( 50.f * abs(p.y  - sx) );
+    dy += 1.f/ (60.f  * length(p - float2(p.x, 0.)));
+    return float4( (p.x + 0.4) * dy, 0.9 * dy, dy, 1.0 );
+}
+
+
+technique Technique1
+{
+    pass Pass1
+    {
+        PixelShader = compile PS_SHADERMODEL main();
+    }
+}
--- ShaderTextureTest/Game1.cs
+++ ShaderTextureTest/Game1.cs
@@ -9,6 +9,11 @@ namespace ShaderTextureTest {
   public class Game1 : Game {
     GraphicsDeviceManager graphics;
     SpriteBatch spriteBatch;
+    Effect myEff;
+    Texture2D bgTest;
+    private RenderTarget2D ShaderRenderTarget;
+    private SpriteBatch secondarySpriteBatch;
+    private Texture2D dummyTexture;
 
     public Game1() {
       graphics = new GraphicsDeviceManager(this);
@@ -23,7 +28,7 @@ namespace ShaderTextureTest {
     /// </summary>
     protected override void Initialize() {
       // TODO: Add your initialization logic here
-
+     
       base.Initialize();
     }
 
@@ -34,8 +39,26 @@ namespace ShaderTextureTest {
     protected override void LoadContent() {
       // Create a new SpriteBatch, which can be used to draw textures.
       spriteBatch = new SpriteBatch(GraphicsDevice);
+      myEff = Content.Load<Effect>("ShaderTest");
+      bgTest = Content.Load<Texture2D>("bgtest");
+      Vector2 buffSize = new Vector2(320, 240);
+      ShaderRenderTarget = createRenderTarget(GraphicsDevice, (int)buffSize.X, (int)buffSize.Y);
+      secondarySpriteBatch = new SpriteBatch(GraphicsDevice);
+      dummyTexture = new Texture2D(GraphicsDevice, (int)buffSize.X, (int)buffSize.Y);
+      Color[] colors = new Color[(int)(buffSize.X * buffSize.Y)];
+      for (int i = 0; i < colors.Length; i++) {
+        colors[i] = Color.Wheat;
+      }
+      dummyTexture.SetData(colors);
+
+      myEff.Parameters["resolution"].SetValue(buffSize);
 
       // TODO: use this.Content to load your game content here
+    }
+
+    public static RenderTarget2D createRenderTarget(GraphicsDevice device, int width, int height) {
+      return new RenderTarget2D(device, width, height, false, SurfaceFormat.Color, DepthFormat.None, 0, RenderTargetUsage.PreserveContents);
+      // bool mipMap, SurfaceFormat format, DepthFormat depthFormat, int preferredMultiSampleCount, RenderTargetUsage usage, SurfaceType surfaceType);
     }
 
     /// <summary>
@@ -54,10 +77,21 @@ namespace ShaderTextureTest {
     protected override void Update(GameTime gameTime) {
       if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
         Exit();
-
-      // TODO: Add your update logic here
-
+      //drawFrameBuffer(gameTime);  //  This does not work
       base.Update(gameTime);
+    }
+
+    private void drawFrameBuffer(GameTime gameTime) {
+      GraphicsDevice.SetRenderTarget(ShaderRenderTarget);
+      GraphicsDevice.Clear(Color.CornflowerBlue);
+
+      myEff.Parameters["time"].SetValue((float)gameTime.TotalGameTime.TotalSeconds);
+      secondarySpriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+      myEff.CurrentTechnique.Passes[0].Apply();
+      secondarySpriteBatch.Draw(dummyTexture, new Rectangle(0, 0, ShaderRenderTarget.Width, ShaderRenderTarget.Height), Color.White);
+      secondarySpriteBatch.End();
+
+      GraphicsDevice.SetRenderTarget(null);
     }
 
     /// <summary>
@@ -68,6 +102,11 @@ namespace ShaderTextureTest {
       GraphicsDevice.Clear(Color.CornflowerBlue);
 
       // TODO: Add your drawing code here
+      spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+      //drawFrameBuffer(gameTime);  //  This does work
+      spriteBatch.Draw(bgTest, new Rectangle(0, 0, bgTest.Width, bgTest.Height), Color.White);
+      spriteBatch.Draw(ShaderRenderTarget, new Rectangle(0, 0, ShaderRenderTarget.Width, ShaderRenderTarget.Height), Color.White);
+      spriteBatch.End();
 
       base.Draw(gameTime);
     }
